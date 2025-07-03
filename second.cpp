626ee45cbb3fda063c8bc9c2874dcf8758525f72
PIYUSHKUMMAR <masterpiyushsh@gmail.com>
2025-07-03 12:44:58 +0530

    another one

---
+++ second.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    float b = 3.14;
+    char c = 'A';
+    double d = 5.554433;
+    bool e = true;
+    cout << "my age: " << a << endl;
+    cout << "pi: "<< b << endl;
+    cout << "my grade: "<< c << endl;
+    cout << "5+0.554433: "<< d << endl;
+    cout << "am i handsome: "<< e << endl;
+    return 0;
+}
